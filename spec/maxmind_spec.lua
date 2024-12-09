57a0655328e49b09d4dcdaff7425dd9a21ba4f12
luatoolz <luatoolz@somahorse.com>
2024-12-09 19:25:05 +0300

    fixed ok

--- spec/maxmind_spec.lua
+++ spec/maxmind_spec.lua
@@ -9,12 +9,13 @@ describe("maxmind", function()
     assert.is_function(maxmind)
   end)
   it("positive", function()
-    assert.is_true(is.ip('8.8.8.8'))
+    assert.is_true(is.ip('2.125.160.216'))
 
-    assert.equal('au', maxmind('1.2.3.4'))
-    assert.equal('us', maxmind('8.8.8.8'))
-    assert.equal('us', maxmind('8.8.8.8'))
-    assert.equal('ru', maxmind('91.232.134.83'))
+    assert.equal('bt', maxmind('67.43.156.1'))
+    assert.equal('gb', maxmind('81.2.69.142'))
+    assert.equal('gb', maxmind('81.2.69.143'))
+    assert.equal('gb', maxmind('81.2.69.160'))
+    assert.equal('se', maxmind('89.160.20.112'))
   end)
   it("negative", function()
     assert.is_nil(maxmind('1.1.1.1'))
@@ -28,3 +29,19 @@ describe("maxmind", function()
     assert.is_nil(maxmind(''))
   end)
 end)
+
+--[[
+      "2.125.160.216/29" : {
+      "67.43.156.0/24" : {
+      "81.2.69.142/31" : {
+      "81.2.69.144/28" : {
+      "81.2.69.160/27" : {
+
+      "81.2.69.192/28" : {
+      "89.160.20.112/28" : {
+      "89.160.20.128/25" : {
+      "175.16.199.0/24" : {
+      "202.196.224.0/20" : {
+      "216.160.83.56/29" : {
+      "214.78.0.0/19" : {
+--]]
