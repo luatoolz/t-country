7b1239b03254f9ed5ad6f515dc1d506b7b6afee0
luatoolz <luatoolz@somahorse.com>
2025-02-04 13:41:47 +0300

    fix maxmind

--- lua/t/maxmind.lua
+++ lua/t/maxmind.lua
@@ -1,5 +1,6 @@
 local t=t or require "t"
 local pkg, is = ..., t.is
+local call = t.call
 local city = "/usr/share/GeoIP/GeoLite2-City.mmdb"
 pkg:assert(is.file(city), 'GeoDB not found', city)
 local mm = assert(require "maxminddb")
@@ -8,7 +9,7 @@ local db = assert(mm.open(city))
 local ip = t.net.ip
 local dns = t.net.dns
 local normalize = function(x) return (type(x)=='string' and x~='') and string.lower(x) or nil end
-local lookup = t.caller(function(it) return it and db:lookup(tostring(it)):get("country", "iso_code") end)
+local lookup = call.caller(function(it) return it and db:lookup(tostring(it)):get("country", "iso_code") end)
 return function(it)
   if is.ip(it) then return normalize(lookup(ip(it))) end
   if is.array(it) then error('ARRAY') end
